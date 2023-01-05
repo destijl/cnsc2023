0e7cb708f62b0681e7447053a951e97db95300ae
destijl <github@mailgreg.com>
2023-01-05 15:48:02 -0800

    Added gatekeeper repo constraint.

--- repo_policy/repo_policy.sh
+++ repo_policy/repo_policy.sh
@@ -2,6 +2,11 @@
 
 source ../shell_setup.sh
 
+p "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml"
+p "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper-library/master/library/general/allowedrepos/template.yaml"
+pe "cat allowed_repos.yaml"
+p "kubectl apply -f allowed_repos.yaml"
+
 pe "kubectl run deny --image=registry.hub.docker.com/library/busybox -- /bin/sleep 60" || true
 pe "kubectl run allow --image=gcr.io/google-containers/busybox -- /bin/sleep 60"
 pe "kubectl get pods"
