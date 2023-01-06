ca3bb602950795aab6ece61a5f567b602433996d
destijl <github@mailgreg.com>
2023-01-05 17:54:28 -0800

    Set gatekeeper to fail closed to flush out errors.

--- repo_policy/repo_policy.sh
+++ repo_policy/repo_policy.sh
@@ -4,6 +4,8 @@ source ../shell_setup.sh
 
 p "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml"
 p "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper-library/master/library/general/allowedrepos/template.yaml"
+pe "cat fail_closed.yaml"
+p "kubectl patch validatingwebhookconfigurations.admissionregistration.k8s.io gatekeeper-validating-webhook-configuration --patch-file fail_closed.yaml"
 pe "cat allowed_repos.yaml"
 p "kubectl apply -f allowed_repos.yaml"
 
