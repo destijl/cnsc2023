a53b137040cb79dd8ffd91cacbe2e287416b74f9
Wes Panther <wpanther@google.com>
2023-01-23 11:46:57 -0800

    Cleanup, fix typo

--- golang_vulns/govulncheck_demo.sh
+++ golang_vulns/govulncheck_demo.sh
@@ -2,12 +2,12 @@
 
 source ../shell_setup.sh
 
-
-p "Image pushed to us-central1-docker.pkg.dev/wpanther-gke-dev/wpanther-test/badimage:v1"
-
 cd used_lib
 
 pe "cat main.go"
+
+# Don't actually push, image is already there
+p "Image pushed to us-central1-docker.pkg.dev/wpanther-gke-dev/wpanther-test/badimage:v1"
 
 pe "go1.18.1 build -o badapp1"
 
