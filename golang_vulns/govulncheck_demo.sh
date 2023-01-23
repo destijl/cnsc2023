1edc7584686fef426a6919db211ec72d19939285
Wes Panther <wpanther@google.com>
2023-01-23 11:53:13 -0800

    Swallow exit code for govulncheck

--- golang_vulns/govulncheck_demo.sh
+++ golang_vulns/govulncheck_demo.sh
@@ -6,11 +6,13 @@ cd used_lib
 
 pe "cat main.go"
 
+pe "go1.18.1 build -o badapp1"
+
 # Don't actually push, image is already there
 p "Image pushed to us-central1-docker.pkg.dev/wpanther-gke-dev/wpanther-test/badimage:v1"
 
-pe "go1.18.1 build -o badapp1"
-
-pe "govulncheck badapp1"
+pe "govulncheck badapp1 || true"
 
 rm badapp1
+
+p ""
