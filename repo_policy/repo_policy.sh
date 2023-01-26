9eab31758d67d7f153da5957705530f0c6762b0a
destijl <github@mailgreg.com>
2023-01-25 17:33:42 -0800

    Repo policy working on GKE standard.

--- repo_policy/repo_policy.sh
+++ repo_policy/repo_policy.sh
@@ -2,12 +2,10 @@
 
 source ../shell_setup.sh
 
-p "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml"
-p "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper-library/master/library/general/allowedrepos/template.yaml"
-pe "cat fail_closed.yaml"
-p "kubectl patch validatingwebhookconfigurations.admissionregistration.k8s.io gatekeeper-validating-webhook-configuration --patch-file fail_closed.yaml"
+pe "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml"
+pe "kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper-library/master/library/general/allowedrepos/template.yaml"
 pe "cat allowed_repos.yaml"
-p "kubectl apply -f allowed_repos.yaml"
+pe "kubectl apply -f allowed_repos.yaml"
 
 pe "kubectl run deny --image=registry.hub.docker.com/library/busybox -- /bin/sleep 60" || true
 pe "kubectl run allow --image=gcr.io/google-containers/busybox -- /bin/sleep 60"
@@ -15,3 +13,8 @@ pe "kubectl get pods"
 
 kubectl delete pods allow --wait=false &> /dev/null
 kubectl delete pods deny --wait=false &> /dev/null || true
+
+#Debugging policy not working
+#pe "cat fail_closed.yaml"
+#p "kubectl patch validatingwebhookconfigurations.admissionregistration.k8s.io gatekeeper-validating-webhook-configuration --patch-file fail_closed.yaml"
+
